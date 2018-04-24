32b7e1c2e83f07b9163eb738da169a4a30cf2dce
S.P. Mohanty <spmohanty91@gmail.com>
2018-04-24 04:07:59 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 docker pull minzwon/dl4mir:gpu-py2
-nvidia-docker run --rm -it -v `pwd`/data/crowdai_fma_test:/crowdai-payload\
-	-v `pwd`:/www\
-	-v /tmp:/tmp\
-	minzwon/dl4mir:gpu-py2 /www/prd.sh
+docker run -v `pwd`/data/crowdai_fma_test:/crowdai-payload  -v `pwd`:/www -v /tmp:/tmp -it minzwon/dl4mir:gpu-py2  --name wwwmusicalgenrerecognitionchallenge_minzwon_test /www/prd.sh
+docker cp wwwmusicalgenrerecognitionchallenge_minzwon_test:/tmp/output.csv .
