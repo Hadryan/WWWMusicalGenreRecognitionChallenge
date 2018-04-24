c2c3b03bb46e73896302ee0108d7f2ae19d46f7d
S.P. Mohanty <spmohanty91@gmail.com>
2018-04-24 04:11:21 +0200

    Fixc run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# export CONTAINER_NAME="wwwmusicalgenrerecognitionchallenge_minzwon"
+chmod +x run.sh
+chmod +x prd.sh
+
 docker pull minzwon/dl4mir:gpu-py2
-docker run -v `pwd`/data/crowdai_fma_test:/crowdai-payload  -v `pwd`:/www -v /tmp:/tmp -it minzwon/dl4mir:gpu-py2  --name wwwmusicalgenrerecognitionchallenge_minzwon_test /www/prd.sh
-docker cp wwwmusicalgenrerecognitionchallenge_minzwon_test:/tmp/output.csv .
+docker run --name $CONTAINER_NAME -v `pwd`/data/crowdai_fma_test:/crowdai-payload  -v `pwd`:/www -v /tmp:/tmp -it minzwon/dl4mir:gpu-py2  /www/prd.sh
+docker cp $CONTAINER_NAME:/tmp/output.csv .
