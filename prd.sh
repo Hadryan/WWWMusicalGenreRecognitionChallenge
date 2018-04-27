95a9de46557f61cdf4a6c0376090073652d163b4
Minz Won <minz.won@upf.edu>
2018-04-27 11:09:50 +0200

    dday

--- prd.sh
+++ prd.sh
@@ -5,5 +5,20 @@ for i in $(seq 0 9)
 	do
 		nohup python get_spectrogram.py run "$i" > "nohup.$i.out" &
 	done
-python predict.py run --gpu_id "0"
+
+TEN=10
+LIM=1
+while true
+	do echo "checking"
+	NUMJOB=$(pgrep python | wc -l)
+	if [ "$NUMJOB" -gt "$LIM" ];
+		then echo "[$(NUMBJOB)] jobs are ongoing [$(date)]"
+		sleep 10
+	else
+		echo "start training [$(date)]"
+		python predict.py run --gpu_id "0"
+
+	fi
+	done
+
 
